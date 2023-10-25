88f2fdf557f8a5f78f849367630cc02b23632b26
Ben Lloyd Pearson <ben.pearson@linearb.io>
2023-10-25 10:02:04 -0400

    Best Practices
    Here is the related [Jira ticket](https://example.atlassian.net/some/project/task)

--- Standard/best-practices/Main.py
+++ Standard/best-practices/Main.py
@@ -1,1 +1,16 @@
 
+# Define a list of numbers
+numbers = [5, 10, 15, 20, 25]
+
+# Initialize a variable to store the sum of numbers
+total = 0
+
+# Iterate through the list and calculate the sum
+for num in numbers:
+    total += num
+
+# Calculate the average
+average = total / len(numbers)
+
+# Print the result
+print(f"The average of the numbers is: {average}")
